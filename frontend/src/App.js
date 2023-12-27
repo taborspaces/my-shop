11644158411a3611fcf556a3bc1489487782c36b
Mia <mia.steben@outlook.com>
2023-12-26 20:41:43 -0800

    manually added footer and header components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,12 @@
+import Footer from "./components/footer";
+import Header from "./components/header";
+
 const App = () => { 
  return (
   <>
+    <Header />
     <h1>Welcome to my shop!</h1>
+    <Footer />
   </>
  );
  }
